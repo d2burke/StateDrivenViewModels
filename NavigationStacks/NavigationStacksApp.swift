4e474b34dfb14b65055c1b61fe37fcd2f9fd048b
Daniel Burke <burke@vouch.app>
2023-06-16 18:05:03 -0400

    Initial Commit

---
+++ NavigationStacks/NavigationStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksApp.swift
+//  NavigationStacks
+//
+//  Created by Daniel Burke on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
